72a5dc4b4351b9d01edd0f4fab747703a0ce1334
= <phduongit2k1@gmail.com>
2021-04-30 21:04:27 +0700

    Add sign up and forget password activity

--- app/src/main/java/com/example/travelholic/MainActivity.java
+++ app/src/main/java/com/example/travelholic/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.travelholic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        map();
+    }
+
+    private void map() {
+        btnLogin = findViewById(R.id.btn_login);
     }
 }
