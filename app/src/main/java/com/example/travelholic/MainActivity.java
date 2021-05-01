7450fe8560c39ba04cae381737992f4ef64d4793
= <phduongit2k1@gmail.com>
2021-05-01 21:20:22 +0700

    Add tour information block

--- app/src/main/java/com/example/travelholic/MainActivity.java
+++ app/src/main/java/com/example/travelholic/MainActivity.java
@@ -15,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         map();
+//        btnLogin.setOnClickListener(v -> {
+//            Intent intent = new Intent(MainActivity.this, ToursActivity.class);
+//            startActivity(intent);
+//        });
     }
 
     private void map() {
