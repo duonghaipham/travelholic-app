e88edf47fc902c796532dbf67256784abf89d24c
= <phduongit2k1@gmail.com>
2021-05-06 23:41:57 +0700

    Add profile fragment

--- app/src/main/java/com/example/travelholic/MainActivity.java
+++ app/src/main/java/com/example/travelholic/MainActivity.java
@@ -9,7 +9,10 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.travelholic.fragment.HistoryFragment;
 import com.example.travelholic.fragment.MapsFragment;
+import com.example.travelholic.fragment.ProfileFragment;
+import com.example.travelholic.fragment.ToursFragment;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.List;
@@ -29,15 +32,15 @@ public class MainActivity extends AppCompatActivity {
         tlMain = findViewById(R.id.tl_main);
 
         tlMain.addTab(tlMain.newTab().setIcon(R.drawable.ic_list));
-        tlMain.addTab(tlMain.newTab().setIcon(R.drawable.ic_map));
+        tlMain.addTab(tlMain.newTab().setIcon(R.drawable.ic_messenger));
         tlMain.addTab(tlMain.newTab().setIcon(R.drawable.ic_history_clock));
-        tlMain.addTab(tlMain.newTab().setIcon(R.drawable.ic_settings));
+        tlMain.addTab(tlMain.newTab().setIcon(R.drawable.ic_user));
 
         PageAdapter adapter = new PageAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-//        adapter.addFragment(new ToursFragment());
+        adapter.addFragment(new ToursFragment());
         adapter.addFragment(new MapsFragment());
-//        adapter.addFragment(new HistoryFragment());
-//        adapter.addFragment(new SettingFragment());
+        adapter.addFragment(new HistoryFragment());
+        adapter.addFragment(new ProfileFragment());
 
         vpMain.setAdapter(adapter);
         vpMain.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tlMain));
